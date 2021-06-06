29ee95e0cbe282bceb1b456cb5fb4c842bbb8085
ryanpersad <Rpersad0305@gmail.com>
2021-06-06 16:55:35 -0400

    update

--- src/main/java/num_names/App.java
+++ src/main/java/num_names/App.java
@@ -9,53 +9,54 @@ public class App
 {
     static Scanner myObj = new Scanner(System.in);
 
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         int month_num;
-        String month;
-        System.out.println( "Please enter the number of the month." );
+        String month = null;
+        System.out.println("Please enter the number of the month.");
         month_num = myObj.nextInt();
+        if (month_num<=12) {
+            switch (month_num) {
+                case 1:
+                    month = "January";
+                    break;
+                case 2:
+                    month = "February";
+                    break;
+                case 3:
+                    month = "March";
+                    break;
+                case 4:
+                    month = "April";
+                    break;
+                case 5:
+                    month = "May";
+                    break;
+                case 6:
+                    month = "June";
+                    break;
+                case 7:
+                    month = "July";
+                    break;
+                case 8:
+                    month = "August";
+                    break;
+                case 9:
+                    month = "September";
+                    break;
+                case 10:
+                    month = "October";
+                    break;
+                case 11:
+                    month = "November";
+                    break;
+                case 12:
+                    month = "December";
+                    break;
+            }
+            System.out.println("The name of the month is " + month);
+        } else{
+            System.out.println("You are out of range");
 
-        switch (month_num) {
-            case 1:
-                month = "January";
-                break;
-            case 2:
-                month = "February";
-                break;
-            case 3:
-                month = "March";
-                break;
-            case 4:
-                month = "April";
-                break;
-            case 5:
-                month = "May";
-                break;
-            case 6:
-                month = "June";
-                break;
-            case 7:
-                month = "July";
-                break;
-            case 8:
-                month = "August";
-                break;
-            case 9:
-                month = "September";
-                break;
-            case 10:
-                month = "October";
-                break;
-            case 11:
-                month = "November";
-                break;
-            case 12:
-                month = "December";
-                break;
-            default:
-                month = "You are out of Range\nPlease enter a number that corresponds to a month";
         }
-        System.out.println("The name of the month is "+month);
     }
 }
